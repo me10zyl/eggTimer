9db9c340f2a4b8807ddf031ebc743fcd01a4424e
zengyilun <me10zyl@qq.com>
2018-06-12 10:57:35 +0800

    Initial Commit

---
+++ EggTime/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  EggTime
+//
+//  Created by 曾艺伦 on 2018/6/12.
+//  Copyright © 2018年 曾艺伦. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ EggTime/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  EggTime
+//
+//  Created by 曾艺伦 on 2018/6/12.
+//  Copyright © 2018年 曾艺伦. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
